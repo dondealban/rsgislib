129b7a8c6c804d92f109a7110fdd1213574ed329
Don De Alban <dondealban@gmail.com>
2016-08-19 16:18:45 +0800

    Inserted script description and additional notes as comments

---
+++ subset-files.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------
+# This Python script subsets multiple files using a bounding box vector,
+# which is a modified version based on the original script developed by
+# Dr Peter Bunting and Dr Daniel Clewley for the JAXA K&C 20 Global
+# Mangrove Watch Workshop.
+#
+# Script modified by:   Jose Don T. De Alban
+# Date created:         19 Aug 2016
+# Last modified:
+# ----------------------------------------------------------------------
+
