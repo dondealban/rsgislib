fd9d36142922ff32b4c6dd014fa1604d83e2d510
Don De Alban <dondealban@gmail.com>
2016-08-23 09:17:06 +0800

    Insert script description and additional notes as comments

---
+++ calibrate-data.py
@@ -0,0 +1,12 @@
+# ----------------------------------------------------------------------
+# This Python script calibrates L-band SAR mosaic data, both JERS-1 and
+# ALOS/PALSAR, to normalised radar cross-section. This script is a
+# modified version based on the original script developed by Dr Peter
+# Bunting and Dr Daniel Clewley for the JAXA K&C 20 Global Mangrove
+# Watch Workshop.
+#
+# Script modified by:   Jose Don T. De Alban
+# Date created:         23 Aug 2016
+# Last modified:
+# ----------------------------------------------------------------------
+
