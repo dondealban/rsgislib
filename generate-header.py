339c7def139ae453ac54a3885b115986e150a2a5
Don De Alban <dondealban@gmail.com>
2016-08-23 13:47:51 +0800

    Insert script description and additional notes as comments

---
+++ generate-header.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------
+# This Python script generates ENVI header files for L-band SAR
+# mosaic data, both JERS-1 and ALOS/PALSAR. This script is a modified
+# version of the original script developed by Dr Daniel Clewley
+# for the JAXA K&C 20 Global Mangrove Watch Workshop.
+#
+# Script modified by:   Jose Don T. De Alban
+# Date created:         23 Aug 2016
+# Last modified:
+# ----------------------------------------------------------------------
+
