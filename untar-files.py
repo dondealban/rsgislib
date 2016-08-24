017db63526353a2f4d80b62266dccb101d7eb8a3
Don De Alban <dondealban@gmail.com>
2016-08-24 10:56:39 +0800

    Insert script description and additional notes as comments

---
+++ untar-files.py
@@ -0,0 +1,12 @@
+# ----------------------------------------------------------------------
+# This Python script generates ENVI header files for L-band SAR
+# mosaic data, both JERS-1 and ALOS/PALSAR. This script is a modified
+# version of the original script developed by Dr Daniel Clewley
+# for the JAXA K&C 20 Global Mangrove Watch Workshop.
+#
+# Usage: script should be in the same directory as the .tar.gz files.
+#
+# Script modified by:   Jose Don T. De Alban
+# Date created:         24 Aug 2016
+# Last modified:
+# ----------------------------------------------------------------------
