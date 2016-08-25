09f54cdadc2894feee6feeb29b2547c873505bdc
Don De Alban <dondealban@gmail.com>
2016-08-25 17:57:23 +0800

    Test search again for specific files within directory; write file names to text file

--- mosaic-data.py
+++ mosaic-data.py
@@ -32,8 +32,8 @@ fileList = open('filelist.txt', 'w')
 
 # Search for specific files within directories then write file names in text file
 for root, dirs, files in os.walk('.'):
-    files = glob.glob("*HH_F02DAR")
     for targetfile in files:
+        targetfile = glob.glob("*HH_F02DAR")
         filename = os.path.join(targetfile)
         filestring = filename + '\n'
         fileList.write(filestring)
