584c13496ed92965bf55d1bb55a92b898deb5b90
Don De Alban <dondealban@gmail.com>
2016-08-25 13:28:25 +0800

    Insert script description and additional notes as comments

---
+++ mosaic-data.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------------------
+# This Python script creates a mosaic from L-band SAR mosaic tile data,
+# both JERS-1 and ALOS/PALSAR, and saves the mosaicked file in KEA file
+# format. This script is a modified version of the original script
+# developed by Dr Daniel Clewley for the JAXA K&C 20 Global Mangrove
+# Watch Workshop.
+#
+# Usage: script should be in the same directory where the uncompressed
+# mosaic tile folders are located.
+#
+# Script modified by:   Jose Don T. De Alban
+# Date created:         25 Aug 2016
+# Last modified:
+# ----------------------------------------------------------------------
